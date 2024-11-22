30dafec4b8bc2cf15041877e1e9a82ddd4b05ea6
Maxwell <maxwellherron5@gmail.com>
2024-11-21 22:06:52 -0600

    simplified mlp even further lol

--- models/mlp_chord_classifier.py
+++ models/mlp_chord_classifier.py
@@ -4,16 +4,14 @@ import torch.nn as nn
 class MLPChordClassifier(nn.Module):
     def __init__(self, input_size, num_classes):
         super(MLPChordClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_size, 64)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(64, 32)
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(32, num_classes)
+        self.network = nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_classes)
+        )
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
-        x = self.relu2(x)
-        x = self.fc3(x)
+        x = self.network(x)
         return x
