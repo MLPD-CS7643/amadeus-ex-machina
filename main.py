52345dace0628ab4fb000f1cb212736b0a81f8dc
Maxwell <maxwellherron5@gmail.com>
2024-11-25 23:44:39 -0600

    Consolidated billboard dataset code into a class

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import sys
 
-from data.data_loader import prepare_model_data, build_data_loaders
+from data.data_loader import BillboardDataProcessor
 from solver import Solver
-
-import numpy as np
 
 
 if __name__ == "__main__":
     # Pass the path to whatever jams file / associated chroma file you want to test this on
-    jams_path = sys.argv[1]
+    model_config = sys.argv[1]
     chroma_path = sys.argv[2]
-    model_config = sys.argv[3]
-    X_train, X_test, y_train, y_test, scaler, label_encoder = prepare_model_data(
-        jams_path
-    )
 
-    train_loader, test_loader = build_data_loaders(
-        jams_path,
-        save_files=False,
-        batch_size=32,  # FIXME rework the dataloaders to get batch size from the config
-    )
+    data_processor = BillboardDataProcessor(batch_size=128)
 
-    input_size = X_train.shape[1]
-    num_classes = len(np.unique(y_train))
+    print("Processing data and building data loaders...")
+    train_loader, test_loader = data_processor.build_data_loaders()
 
+    input_size = train_loader.dataset.tensors[0].shape[1]
+
+    # Get number of classes from the label encoder
+    num_classes = len(data_processor.label_encoder.classes_)
+
+    # Define model arguments
     model_kwargs = {
         "input_size": input_size,
         "num_classes": num_classes,
     }
 
+    # Initialize your solver or model
     solver = Solver.from_yaml(model_config, **model_kwargs)
 
-    solver.train_and_evaluate(train_loader, test_loader, epochs=50, plot_results=True)
+    # Train and evaluate the model
+    solver.train_and_evaluate(train_loader, test_loader, plot_results=True)
 
+    # For inference, you need the scaler and label encoder
+    # They are already available in data_processor
+    scaler = data_processor.scaler
+    label_encoder = data_processor.label_encoder
+
+    # Run inference using the trained model
     solver.run_inference(
         chroma_path,
         scaler,
