e49ecaba34ae890ec383403b238916def7386a4a
zhutwo <25176312+zhutwo@users.noreply.github.com>
2024-12-07 05:44:18 -0500

    add additional instruments to chordgen using GM sf2

--- datagen/chordgen.py
+++ datagen/chordgen.py
@@ -9,10 +9,10 @@ from zipfile import ZipFile
 from utils.gdrive import download_from_gdrive
 
 JSON_FILE = "chord_ref.json"
-SF2_ARCHIVE = "sf2.zip"
+SF2_ARCHIVE = "FluidR3_GM.sf2"
 
-WAV_DIR = "wav"
-SF2_DIR = "sf2"
+WAV_SUBDIR = "wav"
+SF2_SUBDIR = "sf2"
 
 SAMPLE_RATE = 44100
 BIT_DEPTH = 16
@@ -40,7 +40,106 @@ CHORDS = {
     "7b9": [0, 4, 7, 10, 13]
 }
 
-def generate_all_chords(out_dir, download_sf2, start_octave:int=4, end_octave:int=4, make_dir=False):
+GM_INSTRUMENTS = {
+    0: "acoustic_grand_piano",
+    1: "bright_acoustic_piano",
+    2: "electric_grand_piano",
+    3: "honky-tonk_piano",
+    4: "electric_piano_1",
+    5: "electric_piano_2",
+    6: "harpsichord",
+    7: "clavi",
+    8: "celesta",
+    9: "glockenspiel",
+    10: "music_box",
+    11: "vibraphone",
+    12: "marimba",
+    13: "xylophone",
+    14: "tubular_bells",
+    15: "dulcimer",
+    16: "drawbar_organ",
+    17: "percussive_organ",
+    18: "rock_organ",
+    19: "church_organ",
+    20: "reed_organ",
+    21: "accordion",
+    22: "harmonica",
+    23: "tango_accordion",
+    24: "acoustic_guitar_(nylon)",
+    25: "acoustic_guitar_(steel)",
+    26: "electric_guitar_(jazz)",
+    27: "electric_guitar_(clean)",
+    29: "overdriven_guitar",
+    30: "distortion_guitar",
+    32: "acoustic_bass",
+    33: "electric_bass_(finger)",
+    34: "electric_bass_(pick)",
+    35: "fretless_bass",
+    36: "slap_bass_1",
+    37: "slap_bass_2",
+    38: "synth_bass_1",
+    39: "synth_bass_2",
+    40: "violin",
+    41: "viola",
+    42: "cello",
+    44: "tremolo_strings",
+    45: "pizzicato_strings",
+    46: "orchestral_harp",
+    48: "string_ensemble_1",
+    49: "string_ensemble_2",
+    50: "synthstrings_1",
+    51: "synthstrings_2",
+    52: "choir_aahs",
+    53: "voice_oohs",
+    54: "synth_voice",
+    55: "orchestra_hit",
+    56: "trumpet",
+    57: "trombone",
+    58: "tuba",
+    59: "muted_trumpet",
+    60: "french_horn",
+    61: "brass_section",
+    62: "synthbrass_1",
+    63: "synthbrass_2",
+    64: "soprano_sax",
+    65: "alto_sax",
+    66: "tenor_sax",
+    67: "baritone_sax",
+    68: "oboe",
+    69: "english_horn",
+    70: "bassoon",
+    71: "clarinet",
+    72: "piccolo",
+    73: "flute",
+    74: "recorder",
+    75: "pan_flute",
+    76: "blown_bottle",
+    77: "shakuhachi",
+    78: "whistle",
+    79: "ocarina",
+    80: "lead_1_(square)",
+    81: "lead_2_(sawtooth)",
+    84: "lead_5_(charang)",
+    85: "lead_6_(voice)",
+    88: "pad_1_(new_age)",
+    89: "pad_2_(warm)",
+    90: "pad_3_(polysynth)",
+    91: "pad_4_(choir)",
+    92: "pad_5_(bowed)",
+    104: "sitar",
+    105: "banjo",
+    106: "shamisen",
+    107: "koto",
+    108: "kalimba",
+    109: "bag_pipe",
+    110: "fiddle",
+    111: "shanai",
+    112: "tinkle_bell",
+    113: "agogo",
+    114: "steel_drums",
+}
+
+def generate_all_chords(out_dir, download_sf2, start_octave:int=4, end_octave:int=4, duration=2.0, make_dir=False):
     """
     Generates .wav files for all defined chords using all available SoundFonts.
     Also saves chord_ref.json lookup table with metadata.
@@ -58,16 +157,16 @@ def generate_all_chords(out_dir, download_sf2, start_octave:int=4, end_octave:in
     base_path = Path(out_dir)
     if make_dir:
         base_path.mkdir(parents=True, exist_ok=True)
-
-    sf2_path = base_path / SF2_DIR
-    wav_path = base_path / WAV_DIR
+    sf2_dir = base_path / SF2_SUBDIR
+    wav_dir = base_path / WAV_SUBDIR
+    sf_filepath = sf2_dir / SF2_ARCHIVE
     if download_sf2:
-        __fetch_sf2_archive(sf2_path)
-    soundfont_names = [f[:-4] for f in os.listdir(sf2_path) if f.endswith('.sf2')]
-    if not soundfont_names:
-        print(f"No SoundFonts found in {sf2_path}!")
-        return
-    os.makedirs(wav_path, exist_ok=True)
+        __fetch_sf2_archive(sf2_dir)
+    # soundfont_names = [f[:-4] for f in os.listdir(sf2_dir) if f.endswith('.sf2')]
+    # if not soundfont_names:
+    #     print(f"No SoundFonts found in {sf2_dir}!")
+    #     return
+    os.makedirs(wav_dir, exist_ok=True)
     json_out = {}
     print("Starting chord generation...")
     for octave in range(start_octave, end_octave+1):
@@ -77,21 +176,23 @@ def generate_all_chords(out_dir, download_sf2, start_octave:int=4, end_octave:in
                 midi = __generate_midi_chord(root, intervals)
                 note_name = __note_lookup(root)
                 mid_filename = f"{note_name}{chord_class}_O{octave}"
-                mid_filepath = wav_path / f"{mid_filename}.mid"
+                mid_filepath = wav_dir / f"{mid_filename}.mid"
                 midi.save(mid_filepath)
-                for sf_name in soundfont_names:
-                    wav_filename = f"{mid_filename}_{sf_name}"
-                    wav_filepath = wav_path / f"{wav_filename}.wav"
-                    sf_filepath = sf2_path / f"{sf_name}.sf2"
-                    __synthesize_to_wav(str(mid_filepath.absolute()), str(sf_filepath.absolute()), str(wav_filepath.absolute()), sample_rate=SAMPLE_RATE, bit_depth=BIT_DEPTH)
+                for preset_id, instrument_name in GM_INSTRUMENTS.items():
+                    wav_filename = f"{mid_filename}_{instrument_name}"
+                    wav_filepath = wav_dir / f"{wav_filename}.wav"
+                    # sf_filepath = sf2_dir / f"{instrument_name}.sf2"
+                    __synthesize_to_wav(str(mid_filepath.absolute()), str(sf_filepath.absolute()), str(wav_filepath.absolute()), preset_id=preset_id, seconds_to_generate=duration, sample_rate=SAMPLE_RATE, bit_depth=BIT_DEPTH)
                     json_out[wav_filename] = {
                         "root": note_name,
                         "chord_class": chord_class,
                         "billboard_notation": f"{note_name}:{chord_class}",
                         "octave": octave,
-                        "instrument": sf_name,
+                        "instrument": instrument_name,
+                        "gm_preset_id": preset_id,
                         "filename": f"{wav_filename}.wav",
                         "format": "wav",
+                        "duration(s)": duration,
                         "sample_rate": SAMPLE_RATE,
                         "bit_depth": BIT_DEPTH
                     }
@@ -126,15 +227,15 @@ def __save_json(data:dict, path):
         outfile.write(dumps)
 
 def __fetch_sf2_archive(path:Path):
-    print("Downloading sf2.zip...")
+    print("Downloading sf2...")
     dl_path = path / SF2_ARCHIVE
     download_from_gdrive(SF2_ARCHIVE, str(dl_path.absolute()))
-    print("Extracting sf2.zip...")
-    with ZipFile(dl_path, 'r') as zf:
-        zf.extractall(path)
-    os.remove(dl_path)
+    # print("Extracting sf2.zip...")
+    # with ZipFile(dl_path, 'r') as zf:
+    #     zf.extractall(path)
+    # os.remove(dl_path)
 
-def __synthesize_to_wav(midi_path, soundfont_path, output_file, instrument_id=0, preset_id=0, seconds_to_generate=3, sample_rate=44100, bit_depth=16):   
+def __synthesize_to_wav(midi_path, soundfont_path, output_file, instrument_id=0, preset_id=0, seconds_to_generate=2.0, sample_rate=44100, bit_depth=16):   
     # Initialize the synthesizer and load the soundfont
     synth = tsf.Synth(gain=-3)
     soundfont_id = synth.sfload(soundfont_path)
@@ -150,7 +251,7 @@ def __synthesize_to_wav(midi_path, soundfont_path, output_file, instrument_id=0,
     # Collect audio samples into a list
     audio_samples = []
     samples_per_chunk = 4096  # Number of samples per chunk
-    total_samples = sample_rate * seconds_to_generate  # Total samples to generate
+    total_samples = int(sample_rate * seconds_to_generate)  # Total samples to generate
 
     # Generating and collecting audio samples
     for _ in range(0, total_samples+1, samples_per_chunk):
